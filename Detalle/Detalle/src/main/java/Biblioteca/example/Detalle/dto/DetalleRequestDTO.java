ce7e3bedc5036b6ea68f6d689bbdc075b214d835
Gabrieel <gab.castrop@duocuc.cl>
2026-05-12 21:30:43 -0400

    Update DetalleRequestDTO.java
    prueba, "!"

--- Detalle/Detalle/src/main/java/Biblioteca/example/Detalle/dto/DetalleRequestDTO.java
+++ Detalle/Detalle/src/main/java/Biblioteca/example/Detalle/dto/DetalleRequestDTO.java
@@ -7,6 +7,6 @@ public class DetalleRequestDTO {
     private Long historialId;
     @NotNull(message = "El id del libro es obligatorio")
     private Long libroId;
-    @Size(max = 500, message = "La observación no puede superar los 500 caracteres")
+    @Size(max = 500, message = "La observación no puede superar los 500 caracteres!")
     private String observacion;
 }
