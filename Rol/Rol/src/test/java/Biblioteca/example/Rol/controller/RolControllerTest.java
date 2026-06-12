52cb2fa6e0f17ac50f16b276568d2cc4699dc965
felipe1r <146411140+felipelaradev1@users.noreply.github.com>
2026-06-12 16:49:54 -0400

    Edit: cambio de libreria no utilizada

--- Rol/Rol/src/test/java/Biblioteca/example/Rol/controller/RolControllerTest.java
+++ Rol/Rol/src/test/java/Biblioteca/example/Rol/controller/RolControllerTest.java
@@ -19,7 +19,6 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 @WebMvcTest(RolController.class)
 class RolControllerTest {
 
